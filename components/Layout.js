eb0f1c578721887048d5813a165e61f59085bf2a
Juan De La Cruz <jdelacruz@santaclaratech.es>
2020-11-20 00:51:20 -0500

    fix non secure urls error

--- components/Layout.js
+++ components/Layout.js
@@ -22,6 +22,11 @@ const MyLayout = ({ children }) => {
   return (
     <React.Fragment>
       <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
           rel="stylesheet"
